00a36f0c86c7301bff68367403afbaf4c3094724
Guillermo Piñate <gpinate@gmail.com>
2023-05-02 19:49:56 +0200

    sistemas de rutas

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from '../nopagefound/nopagefound.component';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: '**',
+        component: NopagefoundComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}
